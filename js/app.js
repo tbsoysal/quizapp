9d9d5057231e1bed5c9780729c65f4adc766933c
Tahaberk SOYSAL <berksysl.g@gmail.com>
2024-09-19 16:50:59 +0300

    handle quiz logic

--- js/app.js
+++ js/app.js
@@ -1,55 +1,89 @@
-import Questions from './questions.js';
+import Questions from "./questions.js";
 
-const questionCounterEl = document.querySelector('.question-counter');
-const questionTextEl = document.querySelector('.question');
-const answersTextEl = document.querySelectorAll('.answer .text');
+const questionCounterEl = document.querySelector(".question-counter");
+const questionTextEl = document.querySelector(".question");
+const answersTextEl = document.querySelectorAll(".answer .text");
 
-const answersEl = document.querySelectorAll('.answer');
-const nextBtn = document.querySelector('.next-button');
-const previousBtnEl = document.querySelector('header button');
+const answersEl = document.querySelectorAll(".answer");
+const nextBtn = document.querySelector(".next-button");
+const previousBtnEl = document.querySelector("header button");
 
-
-
-class Quizz {
+class Quiz {
   constructor(Questions) {
     this.questions = Questions;
     this.questionIndex = 0;
     this.currentQuestion = this.questions[this.questionIndex];
     this.correctAnswers = 0;
+    this.startQuiz();
+  }
+
+  startQuiz() {
     this.displayCurrentQuestion();
+    this.addListenersToQuestionButtons();
+  }
+
+  addListenersToQuestionButtons() {
+    answersEl.forEach((el) => {
+      el.addEventListener("click", (e) => {
+        const result = this.checkAnswer(e.currentTarget.innerText);
+        if (result) {
+          e.currentTarget.classList.add("checked");
+        } else {
+          e.currentTarget.classList.add("falsechecked");
+          setTimeout(() => {
+            this.showCorrectAnswer();
+          }, 500);
+        }
+      });
+    });
+
+    nextBtn.addEventListener("click", () => this.nextQuestion());
+    previousBtnEl.addEventListener("click", () => this.previousQuestion());
+  }
+
+  showCorrectAnswer() {
+    answersEl.forEach((el) => {
+      if (el.innerText === this.currentQuestion.answer)
+        el.classList.add("checked");
+    });
+  }
+
+  clearAnswerSelections() {
+    answersEl.forEach((el) => {
+      el.classList.remove("checked");
+      el.classList.remove("falsechecked");
+    });
   }
 
   nextQuestion() {
-    if ((this.questionIndex + 1) > 0 && (this.questionIndex + 1) <= 9) {
+    if (this.questionIndex + 1 > 0 && this.questionIndex + 1 <= 9) {
       this.questionIndex++;
       this.currentQuestion = this.questions[this.questionIndex];
     } else {
       return "end";
     }
-    clearAnswerSelections();
+    this.clearAnswerSelections();
     this.displayCurrentQuestion();
   }
 
   previousQuestion() {
-    if ((this.questionIndex - 1) >= 0 && (this.questionIndex - 1) <= 8) {
+    if (this.questionIndex - 1 >= 0 && this.questionIndex - 1 <= 8) {
       this.questionIndex--;
       this.currentQuestion = this.questions[this.questionIndex];
     } else {
       return "end";
     }
-    clearAnswerSelections();
+    this.clearAnswerSelections();
     this.displayCurrentQuestion();
   }
 
   displayCurrentQuestion() {
     // if the current question is not the first question show the previous button
-    if (this.questionIndex > 0)
-      previousBtnEl.style.display = "block";
-    else
-      previousBtnEl.style.display = "none";
+    if (this.questionIndex > 0) previousBtnEl.style.display = "block";
+    else previousBtnEl.style.display = "none";
 
     // Show the current question index
-    questionCounterEl.innerText = `${this.questionIndex + 1}/${this.questions.length}`
+    questionCounterEl.innerText = `${this.questionIndex + 1}/${this.questions.length}`;
 
     // Show the current question
     questionTextEl.innerText = `${this.currentQuestion.question}`;
@@ -57,50 +91,29 @@ class Quizz {
     // Show the answers of the current question
     answersTextEl.forEach((span, index) => {
       span.innerText = this.currentQuestion.answers[index];
-    })
+    });
+
+    if (this.questionIndex === 9) {
+      nextBtn.innerText = "Finish";
+      nextBtn.addEventListener("click", () => this.showScore());
+    }
+  }
+
+  showScore() {
+    document.body.innerHTML = `<h1 style="text-align: center;margin-top: 50%;">Your Score: ${this.correctAnswers * 10} / 100</h1>`;
+    const restartBtn = document.createElement("button");
+    restartBtn.innerText = "Restart";
+    restartBtn.classList.add("next-button");
+    document.body.append(restartBtn);
+    restartBtn.addEventListener("click", () => location.reload());
   }
 
   checkAnswer(answer) {
     if (answer === this.currentQuestion.answer) {
       this.correctAnswers++;
       return true;
-    }
-    else
-      return false;
+    } else return false;
   }
-
 }
 
-const quiz = new Quizz(Questions);
-
-answersEl.forEach((el) => {
-  el.addEventListener('click', (e) => {
-    const result = quiz.checkAnswer(e.target.innerText);
-    if (result) {
-      e.target.classList.add("checked");
-    } else {
-      e.target.classList.add("falsechecked");
-      setTimeout(() => {
-        showCorrectAnswer();
-      }, 500);
-    }
-  });
-})
-
-nextBtn.addEventListener('click', () => quiz.nextQuestion());
-previousBtnEl.addEventListener('click', () => quiz.previousQuestion());
-
-function showCorrectAnswer() {
-  answersEl.forEach(el => {
-    const result = quiz.checkAnswer(el.innerText);
-    if (result)
-      el.classList.add("checked");
-  })
-}
-
-function clearAnswerSelections() {
-  answersEl.forEach(el => {
-    el.classList.remove("checked");
-    el.classList.remove("falsechecked");
-  })
-}
+const quiz = new Quiz(Questions);
